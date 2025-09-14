1b2c84fd48746468a4dd2bb374ce52bce3dc13e1
Cyoyuu <hrandias@gmail.com>
2025-09-13 21:36:26 -0400

    minor

--- agents/meeting_challenge/nav.py
+++ agents/meeting_challenge/nav.py
@@ -236,7 +236,7 @@ class Collector(ThinkingModule):
     def analyze(self, name, position, agent_opinions, places, conversation_history, app_messages, known_eta):
         prompt = open(f"agents/meeting_challenge/meeting_prompts/query_analyze.txt", "r").read()
         prompt = prompt.replace("$SelfName$", name)
-        prompt = prompt.replace("$SelfPosition$", position)
+        prompt = prompt.replace("$AgentInfo$", position)
         prompt = prompt.replace("$AgentOpinions$", agent_opinions)
         prompt = prompt.replace("$Places$", places)
         prompt = prompt.replace("$ConversationHistory$", conversation_history)
@@ -492,7 +492,7 @@ class NavigationMeetingAgent(Agent):
             elif self.discussion_plan["action"]=="wait":
                 action = {"type": "wait"}
             elif self.discussion_plan["action"]=="query":
-                analysis = self.collector.analyze(name=self.name, agents=agents, places=places, conversation_history=conversation_history, app_messages=app_message, known_eta=self.get_known_eta_description())
+                analysis = self.collector.analyze(name=self.name, position=self.obs["agent_pos_dict"], places=places, conversation_history=conversation_history, app_messages=app_message, known_eta=self.get_known_eta_description())
                 collect_plan = self.collector.action(name=self.name, agents=agents, places=places, conversation_history=conversation_history, app_messages=app_message, analysis=f"{analysis}")
                 if collect_plan["target"]==self.name:
                     action = {"type": "query_app", "arg1": "query_route", "arg2": collect_plan["target_locations"][0]}
