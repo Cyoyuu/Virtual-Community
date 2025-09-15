adcfc4deee8f08c25da32480f64f1f0af22bf285
Cyoyuu <hrandias@gmail.com>
2025-09-14 21:51:24 -0400

    minor

--- agents/meeting_challenge/nav.py
+++ agents/meeting_challenge/nav.py
@@ -562,7 +562,7 @@ class NavigationMeetingAgent(Agent):
             rethink_result=self.decider.rethink(curr_time=curr_time, name=self.name, meeting_place=self.meeting_place, curr_eta=curr_eta, eta_history=self.get_eta_history_description())
             self.eta_history[curr_time]=curr_eta
             if rethink_result['initiate_new_discussion']:
-                self.enter_discussion_mode()
+                self.enter_discussion_mode(trigger="RECENT EVENT")
                 action = {"type": "converse", "arg1": rethink_result["speech"], "arg2": 800}
                 return action
         if goal_place == self.obs['current_place'] or (goal_place in self.obs['accessible_places'] and self.s_mem.get_knowledge(goal_place)["building"]=="open space"):
