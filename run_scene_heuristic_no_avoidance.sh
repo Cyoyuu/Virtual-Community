18c888099033c68b52e4aecd3298ace4afaccec7
Xiangye Lin <xiangyelin_umass_edu@gpu048.unity.rc.umass.edu>
2025-11-02 00:36:37 +0000

    experiment scripts

---
+++ run_scene_heuristic_no_avoidance.sh
@@ -0,0 +1,12 @@
+scene=$1
+# Path to your script or command to run
+script_path="ViCo/meeting_challenge/experiment_scripts/run_heuristic_no_avoiding.sh"
+
+for job_id in {1..1}; do
+  echo "Running job_id=$job_id for scene=$scene"
+
+  salloc -p gpu-preempt -G 1 --mem=50G -t 180 --job-name=hn_$scene --nodes=1 srun bash "$script_path" "$scene" "$job_id"
+done
+
+# Optional flags you had commented out:
+# --enable_indoor_activities
--- run_scene_single.sh
+++ run_scene_single.sh
@@ -2,10 +2,10 @@ scene=$1
 # Path to your script or command to run
 script_path="ViCo/meeting_challenge/experiment_scripts/run_single.sh"
 
-for job_id in {0..4}; do
+for job_id in {1..2}; do
   echo "Running job_id=$job_id for scene=$scene"
 
-  salloc -p gpu-preempt -G 1 --mem=100G -t 120 --job-name=single_$scene --constraint="vram80|a40|l40s" srun bash "$script_path" "$scene" "$job_id"
+  salloc -p gpu-preempt -G 1 --mem=100G --nodes=1 -t 120 --job-name=s_$scene --constraint="vram80|a40|l40s" srun bash "$script_path" "$scene" "$job_id"
 done
 
 # Optional flags you had commented out:
