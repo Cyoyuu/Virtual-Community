2a8650bf1052cfdd4628572ba9dcdbeeea91b355
Cyoyuu <hrandias@gmail.com>
2025-11-05 09:06:58 -0500

    add cal_results.sh

---
+++ cal_results.sh
@@ -0,0 +1,6 @@
+python ViCo/meeting_challenge/cal_results.py -o ViCo/meeting/results_stationary_5
+python ViCo/meeting_challenge/cal_results.py -o ViCo/meeting/results_stationary_10
+python ViCo/meeting_challenge/cal_results.py -o ViCo/meeting/results_stationary_20
+python ViCo/meeting_challenge/cal_results.py -o ViCo/meeting/results_patrol_5
+python ViCo/meeting_challenge/cal_results.py -o ViCo/meeting/results_patrol_10
+python ViCo/meeting_challenge/cal_results.py -o ViCo/meeting/results_patrol_20
