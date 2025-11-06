e38110e27b3618564221408ddd7e86ab0a396651
Cyoyuu <hrandias@gmail.com>
2025-11-06 15:19:23 -0500

    set time limit

--- run_scene_heuristic_no_avoidance.sh
+++ run_scene_heuristic_no_avoidance.sh
@@ -4,10 +4,20 @@ sentinel_num=$3
 # Path to your script or command to run
 script_path="ViCo/meeting_challenge/experiment_scripts/run_heuristic_no_avoidance.sh"
 
+if [ "$sentinel_num" -eq 20 ]; then
+  time_limit=240
+elif [ "$sentinel_num" -eq 10 ]; then
+  time_limit=180
+elif [ "$sentinel_num" -eq 5 ]; then
+  time_limit=150
+else
+  time_limit=120  # default fallback
+fi
+
 for job_id in {1..1}; do
   echo "Running job_id=$job_id for scene=$scene"
 
-  salloc -p gpu-preempt -G 1 --mem=50G -t 210 --job-name=hn_$scene --nodes=1 srun bash "$script_path" "$scene" "$sentinel_type" "$sentinel_num" "$job_id"
+  salloc -p gpu-preempt -G 1 --mem=50G -t "$time_limit" --job-name=hn_$scene --nodes=1 srun bash "$script_path" "$scene" "$sentinel_type" "$sentinel_num" "$job_id"
 done
 
 # Optional flags you had commented out:
