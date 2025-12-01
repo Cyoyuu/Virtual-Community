6752db8e1d68a46ed5e8480de4bfbf932e983045
Xiangye Lin <xiangyelin_umass_edu@login3.unity.rc.umass.edu>
2025-12-01 22:51:35 +0000

    last edition

--- cal_results.sh
+++ cal_results.sh
@@ -1,8 +1,17 @@
-python ViCo/meeting_challenge/cal_results.py -r ViCo/meeting_challenge/results_gt/3_stationary_10
-python ViCo/meeting_challenge/cal_results.py -r ViCo/meeting_challenge/results_no_gt/3_stationary_10
-python ViCo/meeting_challenge/cal_results.py -r ViCo/meeting_challenge/results_gt/5_stationary_10
-python ViCo/meeting_challenge/cal_results.py -r ViCo/meeting_challenge/results_no_gt/5_stationary_10
-python ViCo/meeting_challenge/cal_results.py -r ViCo/meeting_challenge/results_gt/3_patrol_10
-python ViCo/meeting_challenge/cal_results.py -r ViCo/meeting_challenge/results_no_gt/3_patrol_10
-python ViCo/meeting_challenge/cal_results.py -r ViCo/meeting_challenge/results_gt/5_patrol_10
-python ViCo/meeting_challenge/cal_results.py -r ViCo/meeting_challenge/results_no_gt/5_patrol_10
+# python ViCo/meeting_challenge/cal_results.py -r ViCo/meeting_challenge/results_gt/3_stationary_10
+# python ViCo/meeting_challenge/cal_results.py -r ViCo/meeting_challenge/results_no_gt/3_stationary_10
+# python ViCo/meeting_challenge/cal_results.py -r ViCo/meeting_challenge/results_gt/5_stationary_10
+# python ViCo/meeting_challenge/cal_results.py -r ViCo/meeting_challenge/results_no_gt/5_stationary_10
+# python ViCo/meeting_challenge/cal_results.py -r ViCo/meeting_challenge/results_gt/3_patrol_10
+# python ViCo/meeting_challenge/cal_results.py -r ViCo/meeting_challenge/results_no_gt/3_patrol_10
+# python ViCo/meeting_challenge/cal_results.py -r ViCo/meeting_challenge/results_gt/5_patrol_10
+# python ViCo/meeting_challenge/cal_results.py -r ViCo/meeting_challenge/results_no_gt/5_patrol_10
+
+python ViCo/meeting_challenge/cal_results.py -r ViCo/meeting_challenge/results_no_gt/10_stationary_10
+python ViCo/meeting_challenge/cal_results.py -r ViCo/meeting_challenge/results_no_gt/10_patrol_10
+python ViCo/meeting_challenge/cal_results.py -r ViCo/meeting_challenge/results_gt/10_stationary_10
+python ViCo/meeting_challenge/cal_results.py -r ViCo/meeting_challenge/results_gt/10_patrol_10
+python ViCo/meeting_challenge/cal_results.py -r ViCo/meeting_challenge/results_gt/5_stationary_5
+python ViCo/meeting_challenge/cal_results.py -r ViCo/meeting_challenge/results_gt/5_patrol_5
+python ViCo/meeting_challenge/cal_results.py -r ViCo/meeting_challenge/results_gt/5_stationary_20
+python ViCo/meeting_challenge/cal_results.py -r ViCo/meeting_challenge/results_gt/5_patrol_20
